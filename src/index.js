0da4f21834df06b917904a7026356589934d64e9
Samuel Tang <samuel@blocksq.com>
2020-06-12 03:12:34 +0800

    Revamp chatbot to use the new set of APIs

--- src/index.js
+++ src/index.js
@@ -3,21 +3,30 @@ const dotenv = require('dotenv');
 const api = require('./api');
 const {
   parseContentFromCtx,
-  fromTopic,
+  fromReport,
   UnsupportedMessageTypeError,
 } = require('./helper/message');
 
 dotenv.config();
 
 const DEFAULT_STATE = {
-  report: { data: null, pendingCreate: false, cancelCreate: false },
+  topic: {
+    message: null,
+    url: null,
+    pendingCreate: false,
+    cancelCreate: false,
+    logFeedback: false,
+  },
 };
 
 module.exports = async function App(context) {
   try {
     const content = parseContentFromCtx(context);
+    if (content.logFeedback) {
+      console.info(JSON.stringify(content));
+    }
     if (content.forceCreate) {
-      await api.createReport(content.data);
+      await api.submitTopic(content.message, content.url);
       await context.sendText('感謝您的反饋。我們會儘快查證此則內容。');
       context.setState(DEFAULT_STATE);
       return;
@@ -28,40 +37,51 @@ module.exports = async function App(context) {
       return;
     }
 
-    const topics = await api.similarTopics(content.data);
-    if (topics.length === 0) {
-      context.setState({
-        report: {
-          data: content.data,
-          pendingCreate: true,
-        },
-      });
+    const { message, url } = content;
+    const reports = await api.searchRelatedReports(message, url);
+    context.setState({ topic: { message, url, pendingCreate: true } });
+
+    if (reports.length === 0) {
       await context.sendButtonTemplate(
         `我們沒有找到相關的消息。請問你想回報此則新聞予我們查證嗎？`,
         [
           {
             type: 'postback',
             title: '好',
-            payload: 'CreateReport',
+            payload: 'CreateTopic',
           },
           {
             type: 'postback',
             title: '先不要',
-            payload: 'CancelCreateReport',
+            payload: 'CancelCreateTopic',
           },
         ]
       );
     } else {
-      const topic = topics[0];
-      const responseText = fromTopic(topic);
+      const report = reports[0];
+      const responseText = fromReport(report);
       await context.sendText(responseText);
+      await context.sendButtonTemplate(`你覺得這個結果正確嗎？`, [
+        {
+          type: 'postback',
+          title: '正確',
+          payload: 'CorrectResponseFeedback',
+        },
+        {
+          type: 'postback',
+          title: '錯誤',
+          payload: 'IncorrectResponseFeedback',
+        },
+      ]);
     }
   } catch (err) {
     if (err instanceof UnsupportedMessageTypeError) {
       await context.sendText('很抱歉，我們暫時只支援文字及連結消息。');
     } else {
       console.error(err);
-      await context.sendText('出現未知錯誤。');
+      await context.sendText(
+        `Factcheck Lab 繁忙中，閣下可到 ${process.env.FACTCHECKLAB_CONTENT_URI} 查找有關的事實查證報告。`
+      );
     }
   }
 };
